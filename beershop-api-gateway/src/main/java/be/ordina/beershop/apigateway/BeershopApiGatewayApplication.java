95cf8f085c1c9ef003b75ac5f48f1340d39cad31
Kristof Eekhaut <kristof.eekhaut@ordina.be>
2019-06-24 11:00:29 +0200

    Add API Gateway using Zuul

---
+++ beershop-api-gateway/src/main/java/be/ordina/beershop/apigateway/BeershopApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.beershop.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeershopApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeershopApiGatewayApplication.class, args);
+    }
+}
---
+++ beershop-api-gateway/src/main/java/be/ordina/beershop/apigateway/config/ZuulConfig.java
@@ -0,0 +1,10 @@
+package be.ordina.beershop.apigateway.config;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableZuulProxy
+public class ZuulConfig {
+
+}
